e7557a16893d93047807f83e08d8f7575af04ce7
noyera <anoyer@10.211.55.4>
2014-01-29 23:03:13 +0100

    ...

--- de.jeandev.api2ecore/src/de/jeandev/api2ecore/Activator.java
+++ de.jeandev.api2ecore/src/de/jeandev/api2ecore/Activator.java
@@ -15,8 +15,7 @@ public class Activator implements BundleActivator {
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
-	public void start(BundleContext bundleContext) throws Exception {
-		
+	public void start(BundleContext bundleContext) throws Exception {	
 		Activator.context = bundleContext;
 	}
 
